40464df5cdf983656cab671867f0b30812e1f71a
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2024-07-02 23:40:01 +0900

    first commit

---
+++ src/main/java/com/example/cafekiosksample/CafekioskSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafekiosksample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafekioskSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafekioskSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cafekiosksample/CafekioskSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafekiosksample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafekioskSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
